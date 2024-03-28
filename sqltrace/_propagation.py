5e150df90130f4b99d0dde0c82b944b154e56334
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2024-03-28 16:39:05 -0600

    tweak impl of handler

--- sqltrace/_propagation.py
+++ sqltrace/_propagation.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 import json
-from datetime import datetime, timezone
 from typing import Any, Protocol, Sequence, overload
 from urllib.parse import unquote_plus
 
